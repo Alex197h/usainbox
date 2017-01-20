e4bab92174ac3500afdecbe4d91d430205590a4b
Quentin Picard <MIW@A-F003744.salsa.univ-amu.fr>
2017-01-20 08:54:34 +0100

    Ajout des routes pour connexion/inscription/déconnexion/oubli mdp/...

--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,21 @@ Route::get('/map', function() {
     return view('map');
 });
 
-Route::any('user/connexion', 'UserController@connexion');
-Route::any('user/deconnexion', 'UserController@deconnexion');
-
-Auth::routes();
-
 Route::get('/home', 'HomeController@index');
+
+
+
+// Authentication Routes...
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::get('logout', 'Auth\LoginController@logout')->name('logout');
+
+// Registration Routes...
+Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+Route::post('register', 'Auth\RegisterController@register');
+
+// Password Reset Routes...
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset');
