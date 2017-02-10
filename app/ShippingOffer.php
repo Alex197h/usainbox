8443c1019bdc22f6b9532c799279e7e9cf53c085
Quentin Picard <MIW@A-F003744.salsa.univ-amu.fr>
2017-02-10 11:52:59 +0100

    modi du modele et de la view shippingoffer

--- app/ShippingOffer.php
+++ app/ShippingOffer.php
@@ -8,12 +8,16 @@ class ShippingOffer extends Model {
     public function user(){
         return $this->belongsTo('App\User');
     }
-    
+
     public function reservations(){
         return $this->belongsTo('App\Reservation');
     }
-    
+
     public function questions(){
         return $this->hasMany('App\Question');
+    }
+
+    public function getVolumeAttribute(){
+        return $this->max_width * $this->max_length * $this->max_height;
     }
 }
--- resources/views/front/shipping/shippinglist.blade.php
+++ resources/views/front/shipping/shippinglist.blade.php
@@ -4,14 +4,41 @@
 
 @section('content')
 
-<div class="container">
-    <div class="starter-template">
+  <div class="row">
+      <div class="col l6 m10 s10 offset-l3 offset-m1 offset-s1 z-depth-1 login-form">
+              <div class="section">
         @foreach($shippings as $shipping)
-            <p>Posté par {{$shipping->user->first_name}} {{$shipping->user->last_name}}
-                {{$shipping->max_price}}€ max, dépard le {{$shipping->fixed_date}} à {{$shipping->fixed_hour}}.
-            </p>
+            <div class="card-panel cyan lighten-5">
+                <br>
+
+                <b>Posté par:</b>
+                {{$shipping->user->first_name}} {{$shipping->user->last_name}}
+                <br>
+
+                <b>Date:</b> Le {{ date('d/m/Y', strtotime($shipping->fixed_date)) }} à {{ date('H:i:s', strtotime($shipping->fixed_hour)) }}
+                <br>
+
+                <b>Description:</b>
+                {{ $shipping->description }}
+                <br>
+
+                <b>Volume:</b>
+                {{ $shipping->getVolumeAttribute() }} cm³
+                <br>
+
+                <b>Poid:</b>
+                {{ $shipping->max_weight }} kg
+                <br>
+
+                <b>Prix maximum</b>
+                {{$shipping->max_price}}€
+                <br>
+
+            </div>
+
         @endforeach
+      </div>
     </div>
-</div>
+  </div>
 
 @endsection
