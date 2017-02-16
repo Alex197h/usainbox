9771f6f6829f404cd16f326f1c7e61f67e967d2a
Clara Bouyer <clara.bouyer45@live.fr>
2017-02-16 10:31:43 +0100

    Ajout de la page 404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('title', 'Nous contacter')
+
+  @section('content')
+    <div class="container">
+      <div class="row">
+        <div class="col l6 m8 s12 offset-l3 offset-m2 card-panel">
+          <h3 style="text-align:center;">Oops ! Vous êtes perdu ?</h3>
+          <div class="section">
+            <section class="col s8 offset-s2">
+              <p style="text-align : justify;">Nous sommes désolés mais la page que vous demandez n'existe pas.
+                si vous êtes arrivés ici depuis notre site, merci de le signaler en nous écrivant à <strong>uboxcovoiturage@gmail.com</strong>.
+                <br>
+                Merci ☺</p>
+              </section>
+              <section class="col s6 offset-s3">
+                {{ Html::image('public/img/404.png', 'Lorem Ipsum', array('class' => 'responsive-img')) }}
+              </section>
+            </div>
+          </div>
+        </div >
+
+      @endsection
