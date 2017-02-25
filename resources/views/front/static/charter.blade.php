c4f1659f9488e6d1b90ebc76a80c2484e3216236
Bouyer <jeremie.bouyer@gmail.com>
2017-02-25 02:54:58 +0100

    Ajout de la charte

--- resources/views/front/static/charter.blade.php
+++ resources/views/front/static/charter.blade.php
@@ -7,7 +7,36 @@
     <div class="row">
         <div class="col s12 ">
             <h3>Charte de bonne conduite</h3>
-
+            <div class="col s9 offset-s1 card-panel">
+                <div class="row what">
+                    <div class="col s6">
+                        <h4>Faire <br> des économies</h4>
+                        <p><i class="material-icons">euro_symbol</i></p>
+                        <p>Inscrivez-vous et indiquez le départ et la destination du colis, ainsi que ses dimensions et poids</p>
+                    </div>
+                    <div class="col s6">
+                        <h4>Faire <br> de l'écologie</h4>
+                        <p><i class="material-icons">local_florist</i></p>
+                        <p>
+                            D'une façon ou d'une autre, le colis sera transporté :
+                            autant qu'un seul véhicule fasse le trajet et pas 2 en même temps !</p>
+                    </div>
+                </div>
+                <div class="row what">
+                    <div class="col s6">
+                        <h4>Faire <br> des économies</h4>
+                        <p><i class="material-icons">euro_symbol</i></p>
+                        <p>Inscrivez-vous et indiquez le départ et la destination du colis, ainsi que ses dimensions et poids</p>
+                    </div>
+                    <div class="col s6">
+                        <h4>Faire <br> de l'écologie</h4>
+                        <p><i class="material-icons">local_florist</i></p>
+                        <p>
+                            D'une façon ou d'une autre, le colis sera transporté :
+                            autant qu'un seul véhicule fasse le trajet et pas 2 en même temps !</p>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
