d97f584c1d476b7185343cc9c2a6f3b9c54f7786
Adrien Michel <adrien.michel96@gmail.com>
2017-03-01 10:19:56 +0100

    Update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\TransportOffer;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,26 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $offers = TransportOffer::all();
+
+        $transport_offers = [];
+        foreach($offers as $offer){
+            if($offer->steps){
+                foreach($offer->steps as $k => $step){
+                    $transport_offers[$step->transport_offer_id][$k] = [
+                        'lng' => $step->longitude,
+                        'lat' => $step->latitude,
+                        'label' => $step->label,
+                    ];
+                }
+            }
+        }
+        return view('front.pages.accueil', ['transport_offers' => $transport_offers]);
+
+    }
+
+    public function ptest(){
+        $offer = TransportOffer::whereIn('id', $_POST['transport'])->get();
+        echo json_encode($offer->toArray());
     }
 }
