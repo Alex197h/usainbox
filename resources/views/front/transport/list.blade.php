319652c7411be436902784e69af0e687a09a1d44
Clara Bouyer <clara.bouyer45@live.fr>
2017-03-08 17:24:53 +0100

    Ajout de la mise en forme pour la recherche

--- resources/views/front/transport/list.blade.php
+++ resources/views/front/transport/list.blade.php
@@ -3,113 +3,138 @@
 @section('title', 'Liste des Offres')
 
 @section('content')
-<div class="row">
-    <form style="background: rgba(120, 106, 106, 0.75);" class="col s10 offset-s1" role="form" method="POST" action="{{ route('transport') }}">
-        {{ csrf_field() }}
-        <h4 class="col s12 white-text">Envoyez vos colis rapidement</h4>
-        <div class="row">
-            <div class="input-field col s12 l3">
-                <input id="city_start" class="white" placeholder="Ville départ" type="text"
-                value="{{ $city_start }}" name="city_start">
-                <label style="display: none;" for="city_start">Ville départ</label>
+    <div class="row rechercheV card-panel">
+        <form style="background: rgba(120, 106, 106, 0.75);" class="col s10 offset-s1" role="form" method="POST" action="{{ route('transport') }}">
+            {{ csrf_field() }}
+            <div class="row">
+                <div class="input-field col s12 l3">
+                    <input id="city_start" class="white" placeholder="Ville départ" type="text"
+                    value="{{ $city_start }}" name="city_start">
+                    <label style="display: none;" for="city_start">Ville départ</label>
+                </div>
+                <div style="text-align: center;" class="input-field col s12 l1">
+                    <button type="button" style="height: 44px;" id="switch" class="btn white black-text">
+                        {{
+                            Html::image('public/img/switch.svg',
+                            'Deux flèches pour switch',
+                            array('class' => 'responsive-img icon', 'style' => 'vertical-align: middle;'))
+                        }}
+                    </button>
+                </div>
+                <div class="input-field col s12 l3">
+                    <input id="city_end" class="white" placeholder="Ville arrivée" type="text" value="{{ $city_end }}"
+                    name="city_end">
+                    <label style="display: none;" for="city_end">Ville arrivée</label>
+                </div>
+                <div class="input-field col s12 l2">
+                    <input type="date" name="date" class=" white" placeholder="Date"
+                    value="{{ date('Y-m-d') }}">
+                </div>
+                <div class="input-field col s12 l3">
+                    <button style="height: 44px;" type="submit" class="col s12 btn waves-effect waves-light white black-text">
+                        Actualiser
+                        {{
+                            Html::image('public/img/recherche/refresh.svg',
+                            'Icon pour actualiser',
+                            array('class' => 'responsive-img iconC', 'style' => 'vertical-align: middle;'))
+                        }}
+                    </button>
+                </div>
             </div>
-            <div style="text-align: center;" class="input-field col s12 l1">
-                <button type="button" style="height: 44px;" id="switch" class="btn white black-text">
-                    {{
-                        Html::image('public/img/switch.svg',
-                        'Deux flèches pour switch',
-                        array('class' => 'responsive-img icon', 'style' => 'vertical-align: middle;'))
-                    }}
-                </button>
-            </div>
-            <div class="input-field col s12 l3">
-                <input id="city_end" class="white" placeholder="Ville arrivée" type="text" value="{{ $city_end }}"
-                name="city_end">
-                <label style="display: none;" for="city_end">Ville arrivée</label>
-            </div>
-            <div class="input-field col s12 l2">
-                <input type="date" name="date" class=" white" placeholder="Date"
-                value="{{ date('Y-m-d') }}">
-            </div>
-            <div class="input-field col s12 l3">
-                <button style="height: 44px;" type="submit" class="col s12 btn waves-effect waves-light white black-text">
-                    Actualiser
-                    {{
-                        Html::image('public/img/recherche/repeat.svg',
-                        'Icon pour actualiser',
-                        array('class' => 'responsive-img icon', 'style' => 'vertical-align: middle;'))
-                    }}
-                </button>
-            </div>
-        </div>
-    </form>
-</div>
-<div class="row">
-    @if(!$offers->isEmpty())
-    @foreach($offers as $offer)
-    <div class="col l6 m10 s10 offset-l3 offset-m1 offset-s1 card-panel">
-        <div class="section center">
-            <h5>{!! ucfirst(utf8_encode(strftime('%A %d %B', strtotime($offer->date_start)))) !!}</h5>
-            @if(isset($steps[$offer->id]))
-            @foreach($steps[$offer->id] as $step)
+        </form>
 
-            <span>{{ $step }}  @if(!$loop->last) → @endif </span>
+        @if(!$offers->isEmpty())
+            @foreach($offers as $offer)
+                <div class="col l6 m10 s10 offset-l3 offset-m1 offset-s1 card-panel annoncepanel">
+                    <div class="section center">
+                        <h5>{!! ucfirst(utf8_encode(strftime('%A %d %B', strtotime($offer->date_start)))) !!}</h5>
+                        @if(isset($steps[$offer->id]))
+                            @foreach($steps[$offer->id] as $step)
 
-            @endforeach
-            @endif
-        </div>
-        <div class="section">
-            @if($offer->is_regular)
-            <i class="small material-icons tooltipped" data-tooltip="Trajet régulier">restore</i>
-            @else
-            <i class="small material-icons tooltipped" data-tooltip="Trajet occasionnel">schedule</i>
-            @endif
-            @if($offer->highway)
-            <i class="small material-icons tooltipped" data-tooltip="Autoroute">surround_sound</i>
-            @endif
-            <br>
+                                <span>{{ $step }}  @if(!$loop->last) → @endif </span>
 
-            <b>Heure de départ:</b> {{ date('H:i', strtotime($offer->date_start)) }}
-            <br>
+                                @endforeach
+                            @endif
+                        </div>
+                        <div class="section">
+                            @if($offer->is_regular)
+                                {{ Html::image('public/img/trajet/regularYes.svg',
+                                    'Calendrier',
+                                    array('class' => 'responsive-img tooltipped iconT', 'data-tooltip' => 'Trajet régulier'))
+                                }}
+                            @else
+                                {{ Html::image('public/img/trajet/regularNo.svg',
+                                    'Calendrier',
+                                    array('class' => 'responsive-img tooltipped iconT', 'data-tooltip' => 'Trajet occasionnel'))
+                                }}
+                            @endif
+                            @if($offer->highway)
+                                {{ Html::image('public/img/trajet/highwayYes.svg',
+                                    'Icon de l\'autoroute',
+                                    array('class' => 'responsive-img tooltipped iconT',
+                                    'data-tooltip' => 'Prend l\'autoroute'))
+                                }}
+                            @endif
+                            @if($offer->detour)
+                                {{ Html::image('public/img/trajet/detour.svg',
+                                    'Icon de deux flèche pour le détour',
+                                    array('class' => 'responsive-img tooltipped iconT',
+                                    'data-tooltip' => 'Détour possible'))
+                                }}
+                            @endif
+                            <br>
 
-            <b>Volume:</b> {{ $offer->volume }}
-            <br>
+                            <b>Heure de départ :</b> {{ date('H:i', strtotime($offer->date_start)) }}
+                            <br>
 
-            <b>Description:</b>
-            {{ $offer->description }}
-            <br>
+                            <b>Volume disponible :</b> {{ $offer->volume }}
+                            <br>
 
-            <b>Détour:</b>
-            {{ $offer->start_detour ? 'Aller' : '' }}
-            {{ $offer->end_detour ? 'Retour' : '' }}
-            <br>
-        </div>
-    </div>
+                            <b>Description :</b>
+                            {{ $offer->description }}
+                            <br>
+                        </div>
+                    </div>
+                </div>
+    <div class="row">
     @endforeach
     <div class="col l6 m10 s10 offset-l3 offset-m1 offset-s1 card-panel">
         <div class="section center">
-            <h4>Aucune offre ne vous convienttttt ?</h4>
+            <h4>Aucune offre ne vous convient ?</h4>
             <form method="post" action="{{ route('create_alert') }}">
                 <input type="hidden" value="{{ $city_start }}" name="city_start">
                 <input type="hidden" value="{{ $city_end }}" name="city_end">
-                <button class="btn btn-primary">Créer une alerte</button>
+                <button class="btn btn-primary btnValider">
+                    Créer une alerte
+                    {{
+                        Html::image('public/img/recherche/bell.svg',
+                        'Icon d\'une cloche',
+                        array('class' => 'responsive-img iconC', 'style' => 'vertical-align: middle;'))
+                    }}
+                </button>
             </form>
-
         </div>
     </div>
-    @else
+@else
     <div class="col l6 m10 s10 offset-l3 offset-m1 offset-s1 card-panel">
         <div class="section center">
-            <h4>Aucune offre disponible</h4>
+            <h4>Aucune offre n'est disponible</h4>
             <form method="post" action="{{ route('create_alert') }}">
                 {{ csrf_field() }}
                 <input type="hidden" value="{{ $city_start }}" name="city_start">
                 <input type="hidden" value="{{ $city_end }}" name="city_end">
-                <button class="btn btn-primary">Créer une alerte</button>
+                <button class="btn btn-primary btnValider">
+                    Créer une alerte
+                    {{
+                        Html::image('public/img/recherche/bell.svg',
+                        'Icon d\'une cloche',
+                        array('class' => 'responsive-img iconC', 'style' => 'vertical-align: middle;'))
+                    }}
+                </button>
             </form>
         </div>
     </div>
-    @endif
+@endif
 </div>
 <script>
 
