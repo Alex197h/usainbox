8426299953a59049fbba1ecaad887683051be816
Adrien Michel <adrien.michel96@gmail.com>
2017-03-21 16:54:05 +0100

    mute

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => 'hello@example.com',
-        'name' => 'Example',
+        'address' => 'uboxcovoiturage@example.com',
+        'name' => 'Usain Box',
     ],
 
     /*
